aef0ba0569bc6b68a0b4ad2ddc579e60845db57e
Maksym Kovpak <kovpak.maksym@gmail.com>
2024-09-15 19:41:33 +0300

    Refactoring

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -1,13 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT
-  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
-  : env.ASPNETCORE_URLS
-    ? env.ASPNETCORE_URLS.split(';')[0]
-    : 'http://localhost:34239';
+const httpsTarget = env.ASPNETCORE_HTTPS_PORT ?? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`;
+const urlTarget = env.ASPNETCORE_URLS ?? env.ASPNETCORE_URLS.split(';')[0];
+const target = httpsTarget ?? urlTarget ?? 'http://localhost:34239';
 
-const context = ['/weatherforecast'];
+const context = [];
 
 module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {
